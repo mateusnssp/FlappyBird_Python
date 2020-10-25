9aed00437da70e7d7c38c38f3f3731e7dec5d6e6
Mateus Nunes <mateusnss@protonmail.ch>
2020-10-25 15:55:08 -0300

    test.jpg adicionado; escala de background melhorado

--- flappybird.py
+++ flappybird.py
@@ -1,15 +1,17 @@
 import pygame, sys
 
 def draw_floor():
-    screen.blit(flor_surface, (flor_x_position, 600))
-    screen.blit(flor_surface, (flor_x_position + 378, 600))
+    screen.blit(flor_surface, (flor_x_position, 630))
+    screen.blit(flor_surface, (flor_x_position + 378, 630))
 
 pygame.init()
 screen = pygame.display.set_mode((378, 672))
 clock = pygame.time.Clock()
 
-bg_surface = pygame.image.load('assets/background-day.png').convert()
-bg_surface = pygame.transform.scale2x(bg_surface)
+bg_surface = pygame.image.load('./assets/background-night.png').convert()
+bg_surface = pygame.transform.scale(bg_surface, [378, 672])
+#bg_surface = pygame.transform.scale2x(bg_surface)
+
 
 flor_surface = pygame.image.load('./assets/base.png').convert()
 flor_surface = pygame.transform.scale2x(flor_surface)
